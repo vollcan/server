fa14912e28788767c3e011973fe659a1301aa630
Vollcan <henryk.ksiazek@protonmail.com>
2017-05-22 19:51:06 +0200

    Added wrapper for running server with parameters and implemented adding records with POST

--- csvhandler.py
+++ csvhandler.py
@@ -2,7 +2,8 @@ import csv
 import json
 def csvRead(ascending):
     with open('database.csv') as csvfile:
-        reader = csv.DictReader(csvfile)
+        fieldnames = ['name', 'price']
+        reader = csv.DictReader(csvfile, fieldnames=fieldnames)
         if ascending==True:
             sortedlist = sorted(reader, key=lambda row:(
                         row['name'],row['price']),reverse=False)
@@ -14,4 +15,31 @@ def csvRead(ascending):
             print(row['name'], row['price'])
             jsonlist = json.dumps(sortedlist, sort_keys=True)
     return jsonlist
+
+def csvWrite(name, price):
+    with open('database.csv') as csvfile:
+        reader = csv.reader(csvfile,delimiter=',')
+        exists = False
+        for row in reader:
+            if row[0] == name:
+                row[0] = name
+                row[1] = price
+                exists = True
+        if exists == False:
+            with open('database.csv', 'a') as output:
+                writer = csv.writer(output)
+                writer.writerow((name,price))
+            return
+        '''
+        else:
+            with open('database.csv', 'r+') as output:
+                readers = csv.reader(output)
+                writers = csv.writer(output)
+                writers.writerow(('dupa', 'dupa'))
+                for row in readers:
+                    names = row[0]
+                    prices = row[1]
+                    writers.writerow((names,prices))
+                return
+            '''
 
--- server.py
+++ server.py
@@ -1,38 +1,42 @@
 from flask import Flask, request, Response
+from launcher import flaskrun
 from flask_basicauth import BasicAuth
 import csvhandler
+import json
 app = Flask(__name__)
 
-app.config['BASIC_AUTH_USERNAME'] = 'admin'
-app.config['BASIC_AUTH_PASSWORD'] = 'admin'
+#app.config['BASIC_AUTH_USERNAME'] = 'admin'
+#app.config['BASIC_AUTH_PASSWORD'] = 'admin'
 
-basic_auth = BasicAuth(app)
+#basic_auth = BasicAuth(app)
 
 @app.route('/cake', methods=['POST', 'DELETE', 'PUT'])
-@basic_auth.required
+#@basic_auth.required
 def cakeSecret():
     if request.method=='POST':
-        return
+        content = request.get_json()
+
+        name = content['name']
+        price = content['price']
+
+        data = {'name': name, 'price': price}
+        print name
+        print price
+        csvhandler.csvWrite(name, price)
+        return str(data)
     if request.method=='DELETE':
         return
     if request.method=='PUT':
         return
 
-@app.route('/cake', methods=['GET', 'POST', 'DELETE', 'PUT'])
+@app.route('/cake')
 def cake():
-    if request.method=='POST':
-        return "This is %s" % request.form['name']
     if request.method=='GET':
         data = csvhandler.csvRead(True)
         return str(data)
-    if request.method=='DELETE':
-        return "This is delete cake"
-    if request.method=='PUT':
-        return "This is put cake"
 
 @app.route('/ekac')
 def ekac():
     return str(csvhandler.csvRead(False))
 
-if __name__=='__main__':
-    app.run(debug=True)
+flaskrun(app)
